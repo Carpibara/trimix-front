5896201b35acec8542959ae2db08f424c198e3f3
Carpibara <cristian.fer.87@gmail.com>
2023-08-02 13:03:22 -0300

    test red

--- src/layouts/main.jsx
+++ src/layouts/main.jsx
@@ -21,7 +21,7 @@ function NavBar() {
     return (
         <Navbar bg="dark" variant="dark">
             <Container>
-                <Navbar.Brand href="/">Trimix</Navbar.Brand>
+                <Navbar.Brand href="/">Trimix 2</Navbar.Brand>
                 <Nav className="me-auto">
                     <Nav.Link href="/">CRUD Persona</Nav.Link>
                 </Nav>
